55659a95eb7c12b86d143519ae268fc96cb17630
Ateeqa-26 <asifateeqa5@gmail.com>
2026-05-23 17:21:01 +0500

    Delete lib directory

--- lib/core/constants/api_constants.dart
+++
@@ -1,6 +0,0 @@
-class ApiConstants {
-  static const String baseUrl =
-      "https://crudcrud.com/api/a8c9dae37e4144a789a3814d6bdfa474";
-
-  static const String productsEndpoint = '$baseUrl/products';
-}
